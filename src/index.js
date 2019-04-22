fb0deb01c5f0e5d73eec94dcffae0cea92c4d403
Matthew Dzaman <matthew@dzaman.org>
2019-04-22 10:11:26 -0700

    re-add string termination message & limit reads in flight

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ var Readable = require('stream').Readable
 var util = require('util')
 
 // from Babel, for backwards compatibility with node 6.x
-function asyncGeneratorStep(gen, resolve, reject, _next, _throw, key, arg) {
+function asyncGeneratorStep (gen, resolve, reject, _next, _throw, key, arg) {
   try {
     var info = gen[key](arg)
     var value = info.value
@@ -21,17 +21,18 @@ function asyncGeneratorStep(gen, resolve, reject, _next, _throw, key, arg) {
 }
 
 // from Babel, for backwards compatibility with node 6.x
-function _asyncToGenerator(fn) {
+function _asyncToGenerator (fn) {
   return function () {
-    var self = this, args = arguments
+    var self = this
+    var args = arguments
     return new Promise(function (resolve, reject) {
       var gen = fn.apply(self, args)
 
-      function _next(value) {
+      function _next (value) {
         asyncGeneratorStep(gen, resolve, reject, _next, _throw, 'next', value)
       }
 
-      function _throw(err) {
+      function _throw (err) {
         asyncGeneratorStep(gen, resolve, reject, _next, _throw, 'throw', err)
       }
 
@@ -58,42 +59,49 @@ function Walker(dir, options) {
   this.options = options
   if (options.depthLimit > -1) this.rootDepth = this.root.split(path.sep).length + 1
   this.fs = options.fs || require('graceful-fs')
+  this._inFlight = 0;
 }
 
 util.inherits(Walker, Readable)
 
-Walker.prototype._readPath = function () {
-  var _ref = _asyncToGenerator(function* (pathItem) {
-
+Walker.prototype._readPath = (function () {
+  var _ref = _asyncToGenerator(function * (pathItem) {
+    const id = Math.random();
     var statFunction = this.options.preserveSymlinks ? this.fs.lstat : this.fs.stat
     var self = this
 
-    return statFunction(pathItem, function (err, stats) {
-      var item = { path: pathItem, stats: stats }
+    return new Promise((resolve, reject) => {
+      statFunction(pathItem, function (err, stats) {
+        var item = { path: pathItem, stats: stats }
 
-      if (err) {
-        self.emit('error', err, item)
-        return
-      }
-
-      if (!stats.isDirectory() || self.rootDepth &&
-          pathItem.split(path.sep).length - self.rootDepth >= self.options.depthLimit) {
-        return self.push(item)
-      }
-
-      self.fs.readdir(pathItem, function (err, pathItems) {
         if (err) {
           self.emit('error', err, item)
-          return self.push(item)
+          resolve()
+          return
         }
 
-        pathItems = pathItems.map(part => path.join(pathItem, part))
-        if (self.options.filter) pathItems = pathItems.filter(self.options.filter)
-        if (self.options.pathSorter) pathItems.sort(self.options.pathSorter)
-        // faster way to do do incremental batch array pushes
-        self.paths.push.apply(self.paths, pathItems)
+        if (!stats.isDirectory() ||
+            (self.rootDepth && pathItem.split(path.sep).length - self.rootDepth >= self.options.depthLimit)) {
+          resolve(self.push(item))
+          return
+        }
 
-        return self.push(item)
+        self.fs.readdir(pathItem, function (err, pathItems) {
+          if (err) {
+            self.emit('error', err, item)
+            resolve(self.push(item))
+            return
+          }
+
+          pathItems = pathItems.map(part => path.join(pathItem, part))
+          if (self.options.filter) pathItems = pathItems.filter(self.options.filter)
+          if (self.options.pathSorter) pathItems.sort(self.options.pathSorter)
+          // faster way to do do incremental batch array pushes
+          self.paths.push.apply(self.paths, pathItems)
+
+          resolve(self.push(item))
+          return
+        });
       })
     })
   })
@@ -101,18 +109,31 @@ Walker.prototype._readPath = function () {
   return function (_x) {
     return _ref.apply(this, arguments)
   }
-}()
+}())
 
-Walker.prototype._read = _asyncToGenerator(function* () {
+Walker.prototype._read = _asyncToGenerator(function * () {
   let pushResult
+
+  if (this._inFlight > 0) {
+    return;
+  }
 
   try {
     while (this.paths.length > 0 && pushResult !== false) {
       var pathItem = this.paths[this.options.queueMethod]()
-      pushResult = yield this._readPath(pathItem)
+      this._inFlight += 1;
+      pushResult = yield this._readPath(pathItem).then((result) => {
+        this._inFlight -= 1
+
+        if (this.paths.length === 0 && this._inFlight === 0) {
+          this.push(null);
+        }
+
+        return result
+      });
     }
   } catch (err) {
-    this.emit('error', err, { path: pathItem });
+    this.emit('error', err, { path: pathItem })
   }
 })
 
