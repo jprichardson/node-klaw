c4b14d1150779f4fdd85050d8f757998e86c8401
Remco Haszing <remcohaszing@gmail.com>
2022-12-30 19:23:42 +0100

    Support file URLs (#47)
    * Support file URLs
    
    Closes #46
    
    * Document file URLs are supported in the readme

--- tests/walk.test.js
+++ tests/walk.test.js
@@ -1,5 +1,6 @@
 const fs = require('fs')
 const path = require('path')
+const { pathToFileURL } = require('url')
 const test = require('./_test')
 const klaw = require('../')
 const fixtures = require('./fixtures')
@@ -47,6 +48,34 @@ test('should work w/ streams 2/3', function (t, testDir) {
       while ((item = this.read())) {
         items.push(item.path)
       }
+    })
+    .on('error', t.end)
+    .on('end', function () {
+      items.sort()
+      let expected = ['a', 'a/b', 'a/b/c', 'a/b/c/d.txt', 'a/e.jpg', 'h', 'h/i', 'h/i/j',
+        'h/i/j/k.txt', 'h/i/l.txt', 'h/i/m.jpg']
+      expected = expected.map(function (item) {
+        return path.join(path.join(testDir, item))
+      })
+      expected.unshift(testDir)
+
+      t.same(items, expected)
+      t.end()
+    })
+})
+
+test('should work w/ file URLs', function (t, testDir) {
+  fixtures.forEach(function (f) {
+    f = path.join(testDir, f)
+    const dir = path.dirname(f)
+    fs.mkdirSync(dir, { recursive: true })
+    fs.writeFileSync(f, path.basename(f, path.extname(f)))
+  })
+
+  const items = []
+  klaw(pathToFileURL(testDir))
+    .on('data', function (item) {
+      items.push(item.path)
     })
     .on('error', t.end)
     .on('end', function () {
